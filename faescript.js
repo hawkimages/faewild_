b255b8906fab6b9fd44da5e5b9b5f682fd1ed3aa
hawkimages <chrisjohnhawk@gmail.com>
2025-06-20 16:32:17 -0700

    same old radius

--- faescript.js
+++ faescript.js
@@ -68,7 +68,7 @@ if (moonContainer) {
 const carousel = document.getElementById('carousel'),
   texts = ['F', 'A', 'E', 'W', 'I', 'L', 'D'],
   // Responsive radius for all devices //
-  radius = Math.max(window.screenX, 205)
+  radius = Math.max(window.innerWidth, 205)
   itemCount = texts.length,
   carouselContainer = document.querySelector('.carousel-container');
 let rotationY = 0, velocityY = 0, isDragging = false, lastMouseX = 0, lastTimestamp = 0;
